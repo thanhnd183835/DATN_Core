3ac882261452ce7bf8f8b3d2e8e429380065f3b9
thanhnd183835 <ducthanh08061998@gmail.com>
2023-07-29 16:03:40 +0700

    push code chieu 29/7

--- src/controller/search.js
+++ src/controller/search.js
@@ -9,31 +9,20 @@ module.exports.search = async (req, res) => {
       // Truy vấn rỗng, trả về lỗi hoặc thông báo cho người dùng
       return res.status(400).json({ error: 'Empty query' });
     }
-    // Tìm kiếm trong chỉ mục Elasticsearch chung
+
     const { body } = await esClient.search({
       index: ['datn', 'datn-user'],
       body: {
         query: {
-          bool: {
-            should: [
-              { match: { userName: query } },
-              { match: { description: query } },
-              { match: { detailItem: query } },
-              { match: { price: query } },
-              { match: { name: query } },
-
-              // Thêm các trường và truy vấn tại đây nếu bạn muốn tìm kiếm trên các trường khác
-            ],
+          query_string: {
+            query: query,
+            fields: ['name', 'description', 'userName'],
+            default_operator: 'AND',
           },
         },
       },
     });
 
-    // if (!body || !body.hits || !body.hits.hits || body.hits.hits.length === 0) {
-    //   console.log('Không có kết quả phù hợp.');
-    //   return res.status(404).json({ message: 'No matching data found.' });
-    // }
-    console.log(body);
     res.json(body.hits.hits.map((hit) => hit._source));
   } catch (err) {
     console.error('Error searching Elasticsearch:', err);
